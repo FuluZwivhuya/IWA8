839d9407a2d37d5fa7db1f9676d0b3ac330d9a95
FuluZwivhuya <148289949+FuluZwivhuya@users.noreply.github.com>
2024-02-08 19:55:16 +0200

    need to fix sarah spacing

--- scripts.js
+++ scripts.js
@@ -1,43 +1,47 @@
-const leoName = 'Leo Musvaire'
-const leoNumber = '2'
-const leoStreet = 'Church St.'
-const leoPostal = '3105'
-const leoBalance = '-10'
+const leoName = "Leo Musvaire";
+const leoNumber = "2";
+const leoStreet = "Church St.";
+const leoPostal = "3105";
+const leoBalance = "-10";
 
-const sarahName = 'Sarah    '
-const sarahSurname = 'Kleinhans'
-const sarahBalance = '-4582.21000111'
-const sarahNumber = '13'
-const sarahStreet = 'William Close'
-const sarahPostal = '0310'
+const sarahName = "Sarah    ";
+const sarahSurname = "Kleinhans";
+const sarahBalance = "-4582.21000111";
+const sarahNumber = "13";
+const sarahStreet = "William Close";
+const sarahPostal = "0310";
 
 // Only change below this line
 
-
-const address = {};
 const leo = {
-	name : leoName,
-	balance : leoBalance,
-	accessId : "47afb389-8014-4d0b-aff3-e40203d2107f",
-	age : 24,
-    address:{
-    number : leoNumber,
-	street : leoStreet,
-	postalcode : leoPostal,
-}}
+  name: leoName,
+  balance: leoBalance,
+  accessId: "47afb389-8014-4d0b-aff3-e40203d2107f",
+  age: 24,
+  address: {
+    number: leoNumber,
+    street: leoStreet,
+    postalcode: leoPostal,
+  },
+};
 
+const sarahName1 = sarahName.trim();
+const sarahName2 = sarahName1 + '' + sarahSurname;
+const sarah = {
+  name: sarahName2,
+  age: 62,
+  accessId: "6b279ae5-5657-4240-80e9-23f6b635f7a8",
+  balance: sarahBalance,
+  address: {
+    number: sarahNumber,
+    street: sarahStreet,
+    postalCode: sarahPostal,
+    age: 62,
+  },
+};
+console.log(leo.name,Object.keys(leo), leo.address.postalcode);
+console.log(leo.name,Object.values(leo), leo.address.postalcode);
 
-const sarah ={
-	name : sarahName + sarahSurname,
-	age : 62,
-	accessId : "6b279ae5-5657-4240-80e9-23f6b635f7a8",
-	balance : sarahBalance,
-	address : {
-		number : sarahNumber,
-		street : sarahStreet,
-		postalCode : sarahPostal,
-	    age: 62,}
-	}
-console.log (leo.name,leo.address.postalcode);
-	
-console.log(sarah.name,sarah.address.postalCode)
+console.log(sarah.name,Object.keys(sarah), sarah.address.postalCode);
+console.log(sarah.name, Object.values(sarah), sarah.address.postalCode);
+
